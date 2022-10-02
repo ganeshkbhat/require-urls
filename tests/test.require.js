545a045bbba43688944dafd7129f3cbc54dd3f76
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-02 21:35:28 +0530

    [COMMIT] [docs] Adding docs for requireurl

---
+++ tests/test.require.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Package: requireurl
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i requireurl --save
+ * Github: https://github.com/ganeshkbhat/requireurl
+ * npmjs Link: 
+ * File: tests/test.require.js
+ * File Description: Test for the requireurl function
+ * 
+*/

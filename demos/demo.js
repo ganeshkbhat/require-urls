35f0dbf6605d1dbe7da53ccd7379f0dcad422591
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-03 10:21:40 +0530

    [COMMIT] [docs] Adding minor changes to demo file docs

--- demos/demo.js
+++ demos/demo.js
@@ -1,5 +1,17 @@
+/**
+ * 
+ * Package: requireurl
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i require-urls --save
+ * Github: https://github.com/ganeshkbhat/requireurl
+ * npmjs Link: 
+ * File: demos/demo.js
+ * File Description: Using requireurl instead of require to fetch files from git repositories like Github or Bitbucket like repository directly
+ * 
+*/
 
 let requireurls = require("../index");
 let c = requireurls("https://raw.githubusercontent.com/ganeshkbhat/requireurl/8d8681c4a28d64f23fb473064fa86880a0b930ff/index.js");
-c.then(d => console.log("testing", d));
+c.then(d => console.log("Testing require-urls function: ", d));
 

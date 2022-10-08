33769355b6f53013b877ddcd09f45390d4c313b4
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-08 18:29:00 +0530

    [TEMP] [improvement] Minor changes to forceUpdate fetch logic - 6

--- demos/demo.mjs
+++ demos/demo.mjs
@@ -15,7 +15,7 @@
 
 'use strict';
 
-import * as requireurls from "https://github.com/ganeshkbhat/requireurl/blob/main/wrapper.js";
+import * as requireurls from "https://github.com/ganeshkbhat/requireurl/blob/main/index.mjs";
 console.log(requireurls);
 
 requireurls.then(d => console.log("Testing returns", d));

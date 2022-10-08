28292e6423c343e428232509e1e111d84a1a9636
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-08 09:20:25 +0530

    [TEMP] [improvement] Adding loader file for import statement url import. Minor changes to readme and remoteUrl function. Minor changes to function implementation

--- .jscache/raw.githubusercontent.com/ganeshkbhat/requireurl/main/indexw.mjs
+++ .jscache/raw.githubusercontent.com/ganeshkbhat/requireurl/main/indexw.mjs
@@ -1,13 +1,13 @@
 /**
  * 
- * Package: requireurl
+ * Package: require-urls
  * Author: Ganesh B
  * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
  * Install: npm i require-urls --save
  * Github: https://github.com/ganeshkbhat/requireurl
  * npmjs Link: https://www.npmjs.com/package/require-urls
- * File: demos/demo.mjs
- * File Description: Using requireurl instead of require to fetch files from git repositories like Github or Bitbucket like repository directly
+ * File: index.mjs
+ * File Description: Using require-urls instead of require to fetch files from git repositories like Github or Bitbucket like repository directly
  * 
 */
 
@@ -15,8 +15,6 @@
 
 'use strict';
 
-import requireurls from "https://github.com/ganeshkbhat/requireurl/blob/main/indexw.js";
+import requireurls from './wrapper.mjs';
 console.log(requireurls);
-
-requireurls.then(d => console.log("Testing returns", d));
-
+export default requireurls; 

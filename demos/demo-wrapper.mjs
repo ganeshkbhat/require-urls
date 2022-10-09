5b880a5944aa6848b37a120482400acfbf639125
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-09 08:07:04 +0530

    [COMMIT] [improvement] Removing wrapper.js file. Adding minor changes to wrapper.mjs. Adding demos for js to mjs import wrapper

--- demos/demo-wrapper.mjs
+++ demos/demo-wrapper.mjs
@@ -1,3 +1,7 @@
 import requireurls from '../wrapper.mjs';
 
 console.log(requireurls);
+
+// import * as requireurls from "https://github.com/ganeshkbhat/requireurl/blob/main/index.mjs";
+
+requireurls("https://github.com/ganeshkbhat/requireurl/blob/main/index.js").then(d => console.log("Testing returns", d));

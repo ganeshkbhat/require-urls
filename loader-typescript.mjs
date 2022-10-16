81a9a8b6f6fb78ef244503e3bb4d13426a785dca
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-17 00:01:10 +0530

    [COMMIT] [feature] [docs] [improvement] Adding support for import(), and import requireurls from 'https://...'. Adding coffeescript sample loader. Adding demos for import() for cjs .js file.  Adding sample coffeescript, https loaders from node samples (modify and incorporate).

---
+++ loader-typescript.mjs
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Package: require-urls
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i require-urls --save
+ * Github: https://github.com/ganeshkbhat/requireurl
+ * npmjs Link: https://www.npmjs.com/package/require-urls
+ * File: loader-typescript.js
+ * File Description: Using require-urls instead of require to fetch files from git repositories like Github or Bitbucket like repository directly
+ * Reference: https://nodejs.org/api/esm.html#import-statements
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+// loader-typescript.mjs
+

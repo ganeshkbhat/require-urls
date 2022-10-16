f59ff0621b5a5c83a6577f380d94a9f3de7020c7
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-17 01:15:11 +0530

    [COMMIT] [docs] Adding .todo file for plan. Minor changes. Adding minor changes to test files structure and names

--- test/test-require-wrapper-remote.mjs
+++ test/test-require-wrapper-remote.mjs
@@ -15,16 +15,15 @@
 // const expect = require('chai').expect;
 // const traverse = require("../src/traverse");
 
-// describe('test.search.files.js::traverse:fssys:traverse-cli: Test Suite for Traverse Files', function() {
+// describe('test-require-wrapper-commonjs.mjs::require-urls: Test Suite for Traverse Files', function() {
 
-//     let resultSingleArray, resultNestedArray;
+
 //     before(async function(){
-//         resultSingleArray = await traverse.dir("./", true, (d, f) => { return path.join(d, f.name); }, false, (e) => { console.log(e) }, "single");
-//         resultNestedArray = await traverse.dir("./", true, (d, f) => { return path.join(d, f.name); }, false, (e) => { console.log(e) }, "nested");
+//     
 //     });
 
 
-//     describe ('test.search.files.js::traverse:fssys:traverse-cli: [Test A] Test Suite for traversing and searching folders in main repo directory', function() {
+//     describe ('test-require-wrapper-commonjs.mjs::require-urls: [Test A] Test Suite for traversing and searching folders in main repo directory', function() {
 //         // it('[Test B] status', function(done){
 //         //     expect(200).to.equal(200);
 //         //     done();
@@ -35,25 +34,6 @@
 //             done();
 //         });
 
-//         it('[Test B] Search file package.json in main directory', function(done) {
-//             expect(100).to.equal(100);
-//             done();
-//         });
-
-//         it('[Test B] Search file package-lock.json in main directory', function(done) {
-//             expect(100).to.equal(100);
-//             done();
-//         });
-
-//         it('[Test B] Search folder ./node_modules in main directory', function(done) {
-//             expect(100).to.equal(100);
-//             done();
-//         });
-
-//         it('[Test B] Search folder ./test in main directory', function(done) {
-//             expect(100).to.equal(100);
-//             done();
-//         });
 //     });
 
 

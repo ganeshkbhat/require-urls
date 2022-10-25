c0bd002f201f09a3a2a7269d6cfc9814646c14ab
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-25 17:19:46 +0530

    [COMMIT] [bug] Adding minor changes to filelock.js _createFileLockJson and filelock._updateFileLockJsonEntry filelock._deleteFileLockJsonEntry filelock._fileContentSHAHash functions. Enabling filelock.json create and update. Adding a structure for filelock._fileContentSHADeHash. Adding filelock._readFileLockJson demo file

--- demos/src/filelock._readFileLockJson.js
+++ demos/src/filelock._readFileLockJson.js
@@ -16,3 +16,13 @@
 /* eslint no-console: 0 */
 
 'use strict';
+
+const _filelock = require("../../src/filelock.js");
+
+let filelock;
+let filelockOptions = { username: "cgi-js", repository: "cgi-js", repositoryPath: "https://www.github.com/cgi-js/cgi-js", localPath: "C:\\Users\\GB\\Documents\\projects\\requireurl\\.jscache\\github\\cgi-js@cgi-js\\4fd7793\\", commit: "4fd7793", sha: "", tag: "" };
+let fileoptions = { name: "", local: "", remote: "", sha: "", digest: "base64", dependencies: {} };
+
+filelock = _filelock._readFileLockJson(filelockOptions.localPath, { logger: console.log });
+
+console.log("[require-urls] demos/src/filelock._createFileLockJson.js: filelock - ", filelock);

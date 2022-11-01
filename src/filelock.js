a2348ebfde18321f389b04c289d689ccde69b407
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-11-01 09:48:50 +0530

    [hasher-apis] [improvement] [docs] [feature] Adding hasher-apis as a external package. Adding .jscache files as a demo

--- src/filelock.js
+++ src/filelock.js
@@ -25,6 +25,7 @@
 
 const { _getGitCommitNumber, _getGitSHAHash, _getGitTagName, _getGitBranchName } = require("./git.js");
 const { _createFolders, _writeFile } = require("./filesystem.js");
+const { _createSHAHash, _fileContentHash, _fileContentDeHash, _verifySHAHash, _verifyFileContentHash } = require("hasher-apis");
 const { _getRequirePaths } = require("./root.dirs.js");
 const path = require("path");
 
@@ -164,64 +165,64 @@ async function _deleteFileLockEntry(filelockOptions, fileoptions, options) {
 }
 
 
-/**
- *
- *
- * @param {*} data
- * @param {*} digest
- * @return {*} 
- */
-function _createSHAHash(algorithm = "sha256", data, digest, options = { logger: console.log }) {
-    const crypto = require('crypto');
-    var hash = crypto.createHash(algorithm).update(JSON.stringify(data)).digest(digest);
-    options.logger("[require-urls]: filelock.js._createSHAHash: Hash created is ", hash);
-    return hash;
-}
+// /**
+//  *
+//  *
+//  * @param {*} data
+//  * @param {*} digest
+//  * @return {*} 
+//  */
+// function _createSHAHash(algorithm = "sha256", data, digest, options = { logger: console.log }) {
+//     const crypto = require('crypto');
+//     var hash = crypto.createHash(algorithm).update(JSON.stringify(data)).digest(digest);
+//     options.logger("[require-urls]: filelock.js._createSHAHash: Hash created is ", hash);
+//     return hash;
+// }
 
-/**
- *
- *
- * @param { string } hashdata
- * @param { string } [algorithm=["aes-256-ctr", "sha256"]]
- * @param { string } salt
- * @param { string } [digest=['ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex']]
- * @param {*} [options={ logger: console.log }]
- * @return {*} 
- */
-function _fileContentDeHash(hashdata, algorithm = "aes-256-ctr", keyAlgorithm = "sha256", salt, digest = "base64", options = { logger: console.log }) {
-    const crypto = require('crypto');
-    const key = crypto.createHash(keyAlgorithm).update(JSON.stringify(salt)).digest(digest);
-    const key_in_bytes = Buffer.from(key, digest);
+// /**
+//  *
+//  *
+//  * @param { string } hashdata
+//  * @param { string } [algorithm=["aes-256-ctr", "sha256"]]
+//  * @param { string } salt
+//  * @param { string } [digest=['ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex']]
+//  * @param {*} [options={ logger: console.log }]
+//  * @return {*} 
+//  */
+// function _fileContentDeHash(hashdata, algorithm = "aes-256-ctr", keyAlgorithm = "sha256", salt, digest = "base64", options = { logger: console.log }) {
+//     const crypto = require('crypto');
+//     const key = crypto.createHash(keyAlgorithm).update(JSON.stringify(salt)).digest(digest);
+//     const key_in_bytes = Buffer.from(key, digest);
 
-    const decipher = crypto.createDecipheriv(algorithm, key_in_bytes, Buffer.from(hashdata.iv, digest));
-    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hashdata.content, digest)), decipher.final()]);
-    return decrpyted.toString();
-}
+//     const decipher = crypto.createDecipheriv(algorithm, key_in_bytes, Buffer.from(hashdata.iv, digest));
+//     const decrpyted = Buffer.concat([decipher.update(Buffer.from(hashdata.content, digest)), decipher.final()]);
+//     return decrpyted.toString();
+// }
 
-/**
- *
- *
- * @param {*} data
- * @param {*} digest ['ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex']
- * @param {*} [options={ logger: console.log }]
- * 
- * reference: https://attacomsian.com/blog/nodejs-encrypt-decrypt-data
- * 
- */
-function _fileContentHash(data, algorithm = "aes-256-ctr", keyAlgorithm = "sha256", salt, digest = "base64", options = { logger: console.log }) {
-    const crypto = require('crypto');
-    const iv = crypto.randomBytes(16);
-    const key = crypto.createHash(keyAlgorithm).update(JSON.stringify(salt)).digest(digest);
-    const key_in_bytes = Buffer.from(key, digest);
+// /**
+//  *
+//  *
+//  * @param {*} data
+//  * @param {*} digest ['ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'base64url' | 'latin1' | 'binary' | 'hex']
+//  * @param {*} [options={ logger: console.log }]
+//  * 
+//  * reference: https://attacomsian.com/blog/nodejs-encrypt-decrypt-data
+//  * 
+//  */
+// function _fileContentHash(data, algorithm = "aes-256-ctr", keyAlgorithm = "sha256", salt, digest = "base64", options = { logger: console.log }) {
+//     const crypto = require('crypto');
+//     const iv = crypto.randomBytes(16);
+//     const key = crypto.createHash(keyAlgorithm).update(JSON.stringify(salt)).digest(digest);
+//     const key_in_bytes = Buffer.from(key, digest);
 
-    const cipher = crypto.createCipheriv(algorithm, key_in_bytes, iv);
-    const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
+//     const cipher = crypto.createCipheriv(algorithm, key_in_bytes, iv);
+//     const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
 
-    return {
-        iv: iv.toString(digest),
-        content: encrypted.toString(digest)
-    };
-}
+//     return {
+//         iv: iv.toString(digest),
+//         content: encrypted.toString(digest)
+//     };
+// }
 
 /**
  *
@@ -251,22 +252,22 @@ function _verifyFilelockFile(remotePath, options) { }
 function _verifyFilelock(remotePath, options) { }
 
 
-/**
- *
- *
- * @param {*} remotePath
- * @param {*} options
- */
- function _verifySHAHash(remotePath, options) { }
+// /**
+//  *
+//  *
+//  * @param {*} remotePath
+//  * @param {*} options
+//  */
+// function _verifySHAHash(remotePath, options) { }
 
- /**
-  *
-  *
-  * @param {*} remotePath
-  * @param {*} options
-  */
- function _verifyFileContentHash(remotePath, options) { }
- 
+// /**
+//  *
+//  *
+//  * @param {*} remotePath
+//  * @param {*} options
+//  */
+// function _verifyFileContentHash(remotePath, options) { }
+
 
 module.exports._writeFileLock = _writeFileLock;
 module.exports._createSHAHash = _createSHAHash;

c93d45f637fb1cb573fcda1b91ca8a8e5bd0804b
Ganesh Bhat <ganeshsurfs@gmail.com>
2023-06-03 16:08:29 +0530

    [COMMIT] Updating readme and adding new socketsjs as dependency to move to independent package

---
+++ setup.bat
@@ -0,0 +1,120 @@
+cd apis-git
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/apis-git.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd apis-hasher
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/apis-hasher.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd apis-mercurial
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/apis-git.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd apis-svn
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/apis-svn.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd apis-request
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/apis-request.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd concurrency
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/concurrency.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd concurrency.js.extended
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/concurrency.js.extended.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd filelock.json
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/filelock.json.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd fssys
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/traverse-fs/fssys.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd get-imports
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/get-imports.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd get-isesm
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/get-isesm.git
+git remote -v
+git pull origin main
+git checkout main
+cd ..
+cd get-root
+git init
+git remote -v
+git remote remove main
+git remote remove origin
+git remote add origin https://github.com/ganeshkbhat/get-root.git
+git remote -v
+git pull origin main origin main
+git checkout main
+cd ..
